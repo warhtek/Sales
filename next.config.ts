cdfc0aabbd9b5748dc6b66973fe734ea3ed46325
Walter Rivera <warhtek@gmail.com>
2024-12-15 10:10:13 -0600

    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+
+/** @type {import('next').NextConfig} */
+//import type { NextConfig } from 'next';
+
+const nextConfig = {//: NextConfig
+  experimental: {
+    ppr: 'incremental',
+  },
+  /*output: 'export', config options here */
+};
+
+export default nextConfig;
